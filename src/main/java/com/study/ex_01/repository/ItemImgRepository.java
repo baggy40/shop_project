0dfd09870edccfc0ad4c64c19c8c4c162f21ea97
DESKTOP-GO5VDIR\gu_02 <baggy35@naver.com>
2022-09-28 11:12:02 +0900

    2022/09/28 파일 업로드 준비

---
+++ src/main/java/com/study/ex_01/repository/ItemImgRepository.java
@@ -0,0 +1,7 @@
+package com.study.ex_01.repository;
+
+import com.study.ex_01.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+}
